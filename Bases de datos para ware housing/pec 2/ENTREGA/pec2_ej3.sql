ab75fcadeca0d7139fd39399d1a329ac4c95df14
alancobu <80952485+alancobu@users.noreply.github.com>
2021-11-08 07:14:56 +0100

    separacion de archivos de entrega

--- Bases de datos para ware housing/pec 2/ENTREGA/pec2_ej3.sql
+++ Bases de datos para ware housing/pec 2/ENTREGA/pec2_ej3.sql
@@ -1,14 +1,27 @@
 3-----------
 
-a)
+--A)
+
 INSERT INTO 
-	olimpic.tb_athlete(
+	olimpic.tb_athlete
+	(
 		athlete_id, name, 
 		country, 
-		substitute_id) VALUES('0000001','REMBRAND Luc','FRA',NULL);
+		substitute_id
+	) 
+VALUES
+(
+	'0000001',
+	'REMBRAND Luc',
+	'FRA',
+	NULL
+);
+
 INSERT INTO olimpic.tb_athlete(athlete_id, name, country, substitute_id) VALUES('0000002','SMITH Mike','ENG',NULL 
 INSERT INTO olimpic.tb_athlete(athlete_id, name, country, substitute_id) VALUES('0000003','LEWIS Carl','USA',NULL);
-b)
+
+--B)
+
 ALTER TABLE
 	tb_athlete
 ADD CONSTRAINT
@@ -19,3 +32,28 @@ CHECK
 	substitute_id is not NULL
 ) 
 NOT VALID;
+
+--C)
+
+CREATE VIEW
+	olimpic.exercise33
+		(
+			athlete_id,
+			name,
+			country,
+			substitute_id
+		) 
+	AS
+		(
+			SELECT 
+				athlete_id,
+				name,
+				country,
+				substitute_id
+			FROM 
+				tb_athlete
+			WHERE 
+				(country LIKE 'ESP' AND
+				name LIKE 'PE%'	)
+		)
+	WITH CHECK OPTION;
